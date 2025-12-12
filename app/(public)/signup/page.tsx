4cffd481633baba63e846f643c344115f1e4e6a0
satrex <info@satrex.jp>
2025-12-13 07:02:11 +0900

    added default

--- app/(public)/signup/page.tsx
+++ app/(public)/signup/page.tsx
@@ -1,6 +1,6 @@
 import { supabase } from '@/lib/supabaseClient';
 
-export async function signUp(email: string, password: string) {
+export default async function signUp(email: string, password: string) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
