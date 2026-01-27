5b0cac4a1360b14893a4a35b49ebff53db22b6f2
satrex <info@satrex.jp>
2026-01-27 18:58:01 +0900

    extented width

--- app/musician/calendar/CalendarClient.tsx
+++ app/musician/calendar/CalendarClient.tsx
@@ -38,7 +38,7 @@ export default function CalendarClient({ performances }: CalendarClientProps) {
   const month = currentMonth.getMonth();
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto">
       {/* ヘッダー: 前月/次月ボタン */}
       <header className="flex items-center justify-between mb-4">
         <button
