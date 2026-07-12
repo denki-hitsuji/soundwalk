f0be27b23b9c5eead0835860f0733d79ebb33088
satrex <info@satrex.jp>
2026-07-12 19:51:47 +0900

    refactor: item9 remove dead musician dashboard ui

--- app/musician/page.tsx
+++ app/musician/page.tsx
@@ -1,9 +1,7 @@
 "use server"
 import { NextPerformanceSectionServer } from "@/components/performances/NextPerformanceSection.server";
-import { DashboardSongsSection } from "@/components/songs/DashboardSongsSection"
 import { SongSummaryCard } from "@/components/songs/SongSummaryCard";
 import { getCurrentUser } from "@/lib/auth/session.server";
-import DashboardClient from "./DashboardClient";
 import { redirect } from "next/navigation";
 
 export default async function MusicianDashboardPage() {
